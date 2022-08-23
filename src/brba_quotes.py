d899ee097b3b896cf85ad311c308bed1e9f27a01
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-08-23 23:02:13 +0800

    Create brba_quotes.py

---
+++ src/brba_quotes.py
@@ -0,0 +1,19 @@
+from requests import get
+
+
+class BrBaQuotes:
+	def __init__(self):
+		self.api = "https://api.breakingbadquotes.xyz"
+		self.headers = {
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+		}
+
+	def get_random_quote(self):
+		return get(
+			f"{self.api}/v1/quotes"
+			headers=self.headers).json()
+
+	def get_multiple_quotes(self, count: int):
+		return get(
+			f"{self.api}/v1/quotes/{count}"
+			headers=self.headers).json()	
